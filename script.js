2256e82cd29bbdcfc0774a484ed707019ad68c8c
Rugved1218 <214828701+Rugved1218@users.noreply.github.com>
2025-09-18 18:54:33 +0530

    Make getComputerChoice() function to take computer's choice

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-console.log("Hello World")
+function getComputerChoice()
+{
+    a=Math.floor((Math.random())*3)+1;
+    if(a===1){
+        return "rock";
+    }
+    else if(a===2){
+        return "paper";
+    }
+    else{
+        return "scissors";
+    }
+}
+
