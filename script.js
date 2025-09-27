858e10bbc017fc470072e8ec837abc0cbf6922f0
Rugved1218 <214828701+Rugved1218@users.noreply.github.com>
2025-09-27 16:33:51 +0530

    Make Ui by index.html and script.js with css flex remaining

--- script.js
+++ script.js
@@ -70,64 +70,125 @@ let btncontainer = document.querySelector('#button-container');
 humanScore=0;
 computerScore=0;
 
-let resultsdiv=document.createElement('div');
+
+
+let divCompSelection=document.querySelector(".computerSelection");
+let divUserSelection=document.querySelector(".userSelection");
+
+let roundNo=1;
+
 
 btncontainer.addEventListener('click',(event) => {
-    let target=event.target;
-    let computerSelection=getComputerChoice();
-    let roundresult;
-    
-
-    switch(target.id){
-
-        case 'rockbtn' :
-
-            roundresult=playRound("rock",computerSelection);
-            console.log(roundresult)
-
-            resultsdiv.textContent=roundresult;
-            console.log(resultsdiv);
-
-            results.appendChild(resultsdiv);
-            console.log(results);
-
-            break;
-
-        case 'paperbtn':
-
-            roundresult=playRound("paper",computerSelection);
-            console.log(roundresult)
-
-            resultsdiv.textContent=roundresult;
-            console.log(resultsdiv);
-
-            results.appendChild(resultsdiv);
-            console.log(results);
-
-            break;
-
-        case 'scissorsbtn':
-
-            roundresult=playRound("scissors",computerSelection);
-            console.log(roundresult)
-
-            resultsdiv.textContent=roundresult;
-            console.log(resultsdiv);
-
-            results.appendChild(resultsdiv);
-            console.log(results)
-
-            break;
-
+    if(roundNo>5){
+        event.stopPropagation;
+        const final_result=document.querySelector(".final-result");
+        console.log(final_result)
+        if(humanScore>computerScore){
+            final_result.textContent="You Won the Game! 🏆";
+        }
+        else if(humanScore<computerScore){
+            final_result.textContent="You Lost the Game! 💔";
+        }
+        else{
+            final_result.textContent="It's a Tie ⚖️";
+        };
     }
-    console.log("computerSelection : ",computerSelection)
-    console.log("");
-    console.log("User Score after Round : ",humanScore);
-    console.log("Computer Score after Round : ",computerScore);
-    console.log("");
-    console.log("");
+    else{
+    
+        const target=event.target;
+        const computerSelection=getComputerChoice();
+        let roundresult;
+        let userSelection;
 
+        const resultsdivRound=document.createElement('div');
+        const resultsdiv=document.createElement('div');
+        const resultsdivScores=document.createElement('div');
+        const resultsdivUserScore=document.createElement('div');
+        const resultsdivCompScore=document.createElement('div');
+
+        resultsdivRound.textContent=("Round "+ roundNo);
+        results.appendChild(resultsdivRound);
+        
+
+        switch(target.id){
+
+            case 'rockbtn' :
+
+                userSelection="rock";
+                roundresult=playRound(userSelection,computerSelection);
+                console.log(roundresult)
+
+                resultsdiv.textContent=roundresult;
+                console.log(resultsdiv);
+
+                results.appendChild(resultsdiv);
+                console.log(results);
+
+                break;
+
+            case 'paperbtn':
+
+                userSelection="paper";
+                roundresult=playRound(userSelection,computerSelection);
+                console.log(roundresult)
+
+                resultsdiv.textContent=roundresult;
+                console.log(resultsdiv);
+
+                results.appendChild(resultsdiv);
+                console.log(results);
+
+                break;
+
+            case 'scissorsbtn':
+
+                userSelection="scissors";
+                roundresult=playRound(userSelection,computerSelection);
+                console.log(roundresult)
+
+                resultsdiv.textContent=roundresult;
+                console.log(resultsdiv);
+
+                results.appendChild(resultsdiv);
+                console.log(results)
+
+                break;
+
+        }
+
+        divUserSelection.textContent=("User Selected : "+userSelection);
+        
+
+        divCompSelection.textContent=("Computer Selected : " + computerSelection);
+        
+        
+        console.log("User Score after Round : ",humanScore);
+        resultsdivUserScore.textContent=("User Score : " + humanScore);
+        results.append(resultsdivUserScore);
+
+        console.log("Computer Score after Round : ",computerScore);
+        resultsdivCompScore.textContent=("Computer Score : " + computerScore);
+        results.append(resultsdivCompScore);
+            
+        console.log("");
+        console.log("");
+            
+        roundNo++;
+    }
 });
 
+if(roundNo==5){
+    const final_result=document.querySelector(".final-result");
+    console.log(final_results)
+    if(humanScore>computerScore){
+        final_result.textContent("You Won the Game! 🏆");
+    }
+    else if(humanScore<computerScore){
+        final_result.textContent("You Lost the Game! 💔");
+    }
+    else{
+        final_result.textContent("It's a Tie ⚖️");
+    };
+}
 
 
